ccac6f4387da22efb43b992dc91d942b93d90cc1
BuildTools <unconfigured@null.spigotmc.org>
2022-05-14 23:05:38 -0600

    Add comments to code

--- app/src/main/java/com/caleblimb/scrabblelookup/Dictionary.kt
+++ app/src/main/java/com/caleblimb/scrabblelookup/Dictionary.kt
@@ -5,13 +5,19 @@ import java.io.IOException
 import java.io.InputStream
 
 class Dictionary(context: Context) {
+    // Define name of dictionary file
     val fileName: String = "words.txt"
+
+    // Create a set to hold the words
     val words: Set<String>
 
     init {
+        // String to hold file
         var string: String = ""
+        // Each line is a single word
         val delimiter = "\r\n"
         try {
+            // Open and Real File
             val inputStream: InputStream = context.assets.open(fileName)
             val size: Int = inputStream.available()
             val buffer = ByteArray(size)
@@ -20,10 +26,13 @@ class Dictionary(context: Context) {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+        // Populate the set with the words from the file
         words = string.lowercase().split(delimiter).toSet()
     }
 
     fun isWord(word: String): Boolean {
+        // Filter user input to standardize to lowercase and remove white space.
+        // Return true if the input is a word and false if it isn't found.
         return (word.lowercase().filter{!it.isWhitespace()} in words)
     }
 }
--- app/src/main/java/com/caleblimb/scrabblelookup/MainActivity.kt
+++ app/src/main/java/com/caleblimb/scrabblelookup/MainActivity.kt
@@ -9,23 +9,26 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialize the application
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Get the components from the xml to be used in code here
         val userInput = findViewById<EditText>(R.id.editTextInput)
-
         val lookupResultMessage = findViewById<TextView>(R.id.textViewResult)
-
         val dictionary = Dictionary(this)
 
+        // Execute as the user enters text so that they don't need to press a button for results.
         userInput.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // User's Input Word
                 val word = userInput.text.toString()
 
+                // Display the appropriate message depending on if the input is a real word or not.
                 if (dictionary.isWord(word)) {
                     lookupResultMessage.text = word.plus(" is a word :)")
                 }
